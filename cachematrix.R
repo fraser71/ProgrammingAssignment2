f54b28ad9a75d8f9ca99831d49e67e5704d8477c
Fraser Hewson <fraser.hewson@gmail.com>
2014-08-24 12:29:00 +0200

    implemented makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions allow for the creation of a matrix where the invers
+## is cached after it has been calculated. This prevents the re-execution 
+## of a rather expensive operation.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a 'special' matrix object that stores the 
+## matrix as well as its inverce. Use this matrix with cacheSolve() 
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m.orig = matrix()) {
+  m.result <- NULL
+  set <- function(m.set) {
+    m.orig <<- m.set
+    m.result <<- NULL
+  }
+  get <- function() m.orig
+  setresult <- function(result) m.result <<- result
+  getresult <- function() m.result
+  list(set = set, get = get,
+       setresult = setresult,
+       getresult = getresult)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve finds the inverse of the matrix created by makeCacheMatrix(). 
+## The inverse of the matrix is only calculated once. On subsequent calls, the 
+## cached result is used.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  mcache <- NULL
+  mlist <- x
+  
+  m <- mlist$getresult()
+  if(!is.null(m)){
+    print("using cached data")
+    return(m)
+  }
+  data <- mlist$get()
+  result <- solve(data, ...)
+  mlist$setresult(result)
+  result
 }
